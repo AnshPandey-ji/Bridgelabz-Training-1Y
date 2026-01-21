b197ef72c2771070940657aea2557880e6271b53
AnshPandey-ji <anshpandey99033@gmail.com>
2026-01-21 18:02:49 +0530

    Second Assignment of Java Completed by Sarthak Pandey

---
+++ 01-Practice-Problems/WelcomeToBridgelabz.java
@@ -0,0 +1,14 @@
+class WelcomeToBridgelabz{
+    public static void main(String[] args) {
+
+        // Creating Scanner object
+        java.util.Scanner scanner = new java.util.Scanner(System.in);
+
+        // Printing the message
+        System.out.println("Welcome to Bridgelabz!");
+
+        // Closing the scanner
+        scanner.close();
+    }
+}
+
