a10c30cb9fca171abd1ef43a4d9a0100009af584
AnshPandey-ji <anshpandey99033@gmail.com>
2026-02-06 10:16:01 +0530

    Fourth Class Work Of Java is Completed by Sarthak Pandey

--- src/com/gla/strings/GenrateAllSubstring.java
+++ src/com/gla/strings/GenrateAllSubstring.java
@@ -1,4 +1,12 @@
 package com.gla.strings;
 
 public class GenrateAllSubstring {
+    public static void main(String[] args) {
+        String str = "Hello";
+        for(int i=0;i<str.length();i++){
+            for(int j=i+1;j<=str.length();j++){
+                System.out.println(str.substring(i,j));
+            }
+        }
+    }
 }
--- src/com/gla/strings/LC58.java
+++ src/com/gla/strings/LC58.java
@@ -1,4 +1,11 @@
 package com.gla.strings;
 
-public class LC58 {
+class LC58 {
+    public int lengthOfLastWord(String s) {
+        String arr[] = s.split(" ");
+        String lastword = arr[arr.length-1];
+        return lastword.length();
+    }
+    public static void main(String[] args) {
+    }
 }
