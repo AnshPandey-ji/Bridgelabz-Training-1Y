d0fb2f5452ea83ae265f3b05c75c72a6d4e1d594
AnshPandey-ji <anshpandey99033@gmail.com>
2026-02-09 20:56:44 +0530

    Fifth Class Work Of Java is Completed by Sarthak Pandey

--- src/com/gla/strings/LC242.java
+++ src/com/gla/strings/LC242.java
@@ -1,4 +1,24 @@
 package com.gla.strings;
 
-public class LC242 {
+class LC242 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length())
+            return false;
+
+        int[] arr = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            arr[s.charAt(i) - 'a']++;
+            arr[t.charAt(i) - 'a']--;
+        }
+
+        for (int check : arr) {
+            if (check != 0)
+                return false;
+        }
+
+        return true;
+    }
+    public static void main(String[] args) {
+    }
 }
